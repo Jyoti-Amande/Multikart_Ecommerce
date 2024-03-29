34e9e8dcf2390af768ea6a5c6fec97599003b8ba
Jyoti-Amande <Jyotiamande@gemail.com>
2024-03-29 11:48:18 +0530

    HomePage changes

--- multikart_ecommerce_project/src/App.js
+++ multikart_ecommerce_project/src/App.js
@@ -6,19 +6,14 @@ import './App.css';
 //import CarouselComponent from './App/Page/HomePage/Carousel';
 //import Home from './App/Constant/Header/Header-Home-Page/Navbar-Second/Nav-Components/Home';
 import HomePage from './App/Page/HomePage/HomePageMain/HomePage';
-import FashionInsta from './App/Page/HomePage/Instagram/FashionInsta';
-import Service1 from './App/Page/HomePage/Services/Service1';
-import LogoBlock from './App/Page/HomePage/logo-block';
-import {  instagram,   logoImage } from './App/Page/HomePage/dada_InstaPage';
+
 function App() {
  
 
   return (
     <div>
  <HomePage/>
- <Service1/> 
-    <FashionInsta instagram={instagram}/>
-    <LogoBlock logoImage={logoImage}/> 
+ 
  </div>
   );
 }
--- multikart_ecommerce_project/src/App/Page/HomePage/HomePageMain/HomePage.js
+++ multikart_ecommerce_project/src/App/Page/HomePage/HomePageMain/HomePage.js
@@ -3,6 +3,10 @@ import CarouselComponent from '../Carousel'
 import Poster from '../Poster'
 import { slide } from './data_HomePage'
 import { poster } from './data_HomePage'
+import FashionInsta from './App/Page/HomePage/Instagram/FashionInsta';
+import Service1 from './App/Page/HomePage/Services/Service1';
+import LogoBlock from './App/Page/HomePage/logo-block';
+import {  instagram,   logoImage } from './App/Page/HomePage/dada_InstaPage';
 
 
 function HomePage() {
@@ -12,7 +16,9 @@ function HomePage() {
         <div className="App">
           <CarouselComponent slide={slide} />
           <Poster poster={poster} />
-          
+          <Service1/> 
+    <FashionInsta instagram={instagram}/>
+    <LogoBlock logoImage={logoImage}/> 
         </div>
       </div>
   )
